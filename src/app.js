4dc68d35f6795836353946319a2e9cb7536e5d0f
fcarlone <frankcarlone@gmail.com>
2018-12-20 17:08:52 -0500

    Setup styling format

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter, { history } from './routers/AppRouter';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
 import { firebase } from './firebase/firebase';
 
 const jsx = (
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,7 @@
 // Stateless functional component
 import React from 'react';
 import { firebase, googleAuthProvider } from '../firebase/firebase';
+import styles from '../styles/components/LoginPage.scss';
 
 const startLogin = () => {
   return firebase.auth().signInWithPopup(googleAuthProvider)
@@ -10,9 +11,13 @@ const startLogin = () => {
 };
 
 export const LoginPage = () => (
-  <div>
+  <div className={styles.layout}>
     <h1>LoginPage Component</h1>
-    <button onClick={startLogin}>Login</button>
+    <div className={styles.container}>
+      <p>Login here</p>
+      <button onClick={startLogin}>Login</button>
+    </div>
   </div>
 );
 
+export default LoginPage;
