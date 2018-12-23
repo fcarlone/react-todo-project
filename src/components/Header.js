97c19a96f6e5a8810d1786107ccae5e599217646
fcarlone <frankcarlone@gmail.com>
2018-12-23 12:24:56 -0500

    Remove Header component from login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,38 @@ import React from 'react';
 import { firebase } from '../firebase/firebase';
 import styles from '../styles/components/Header.scss';
 
+class Header extends React.Component {
+  startLogout = () => {
+    return firebase.auth().signOut();
+  };
 
-const startLogout = () => {
-  return firebase.auth().signOut();
-};
+  render() {
+    if (window.location.pathname === '/') {
+      return null;
+    } else {
+      return (
+        <header className={styles.header}>
+          <div className={styles.headerContent}>
+            <h1 className={styles.headerTitle}>React Todo App</h1>
+            <button className={styles.buttonLayout} onClick={this.startLogout}>Logout</button>
+          </div>
+        </header>
+      )
+    }
+  }
+}
 
-const Header = () => (
-  <header className={styles.header}>
-    <div className={styles.headerContent}>
-      <h1 className={styles.headerTitle}>React Todo App</h1>
-      <button className={styles.buttonLayout} onClick={startLogout}>Logout</button>
-    </div>
-  </header>
-);
+// const startLogout = () => {
+//   return firebase.auth().signOut();
+// };
+
+// const Header = () => (
+//   <header className={styles.header}>
+//     <div className={styles.headerContent}>
+//       <h1 className={styles.headerTitle}>React Todo App</h1>
+//       <button className={styles.buttonLayout} onClick={startLogout}>Logout</button>
+//     </div>
+//   </header>
+// );
 
 export default Header;
