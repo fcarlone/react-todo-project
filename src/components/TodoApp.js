31258cdd6c546cb0014f924ceee7efef19ffb1db
Frank Carlone III <fcarlone@users.noreply.github.com>
2019-01-03 13:36:48 -0500

    Create TodoApp.js

---
+++ src/components/TodoApp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LoginPage from './LoginPage';
+
+class TodoApp extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>React Todo Application</h1>
+        <LoginPage />
+      </div>
+    )
+  }
+}
+
+export default TodoApp;
