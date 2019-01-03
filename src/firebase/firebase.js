36fc2dcd0a94e0f3009fc28654a666f08d736862
fcarlone <frankcarlone@gmail.com>
2019-01-03 14:06:39 -0500

    Update firebase code

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,7 +5,7 @@ import 'firebase/database';
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: 'https://react-todo-2a51d.firebaseio.com',
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
